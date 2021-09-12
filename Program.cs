57af909408a27f34f8e074590ccc2f3981a25b91
woodrowdeveloper <woodrowdeveloper@gmail.com>
2021-09-11 21:43:34 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace FaceBook_Logon_Negative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://www.facebook.com");
+
+            driver.FindElement(By.Id("email")).SendKeys("woodrowwinter7402@gmail.com");
+            driver.FindElement(By.Id("pass")).SendKeys("Ethan@7403");
+            driver.FindElement(By.Name("login")).Click();
+
+            
+
+            
+
+
+
+        }
+    }
+}
