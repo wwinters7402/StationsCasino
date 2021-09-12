cc913a9adc2202a1b929e174962279ca37f158d7
woodrowdeveloper <woodrowdeveloper@gmail.com>
2021-09-11 22:38:33 -0400

    Change Password back to 7402. Also added element to find search box.

--- Program.cs
+++ Program.cs
@@ -12,11 +12,16 @@ namespace FaceBook_Logon_Negative
 
 
             driver.Manage().Window.Maximize();
+           
             driver.Navigate().GoToUrl("http://www.facebook.com");
 
             driver.FindElement(By.Id("email")).SendKeys("woodrowwinter7402@gmail.com");
-            driver.FindElement(By.Id("pass")).SendKeys("Ethan@7403");
+            driver.FindElement(By.Id("pass")).SendKeys("Ethan@7402");
             driver.FindElement(By.Name("login")).Click();
+
+           
+
+            driver.FindElement(By.ClassName("ijkhr0an pnx7fd3z sgqwj88q hzruof5a pmk7jnqg rnx8an3s fcg2cn6m")).SendKeys("Lacie Winters");
 
             
 
